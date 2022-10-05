2e02d1d9222ad9c713a2502e3da0419c80f3938d
joshua decano <joshuadecano@gmail.com>
2022-10-05 15:06:40 -0700

    started the phrase literal

--- queries/phraseliteral.py
+++ queries/phraseliteral.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from indexing.postings import Posting
 from .querycomponent import QueryComponent
 
@@ -10,11 +11,27 @@ class PhraseLiteral(QueryComponent):
         self.terms = [s for s in terms]
 
     def get_postings(self, index) -> list[Posting]:
+        terms_holder = []
+        answer = []
+        term_length = range(self.terms)
+        i = 1
         for s in self.terms:
-            return terms
-        return None
+            terms_holder[s] = index.get_postings(s)
+        while i < term_length:          # while the index of the second term(starts at index 1) is less than the length of the phrase
+            while terms_holder[i-1] != NULL and terms_holder[i] != NULL:
+                if terms_holder[i-1].doc_id == terms_holder[i].doc_id:
+                    l = []
+                    pp1 = terms_holder[i-1]
+                    pp2 = terms_holder[i]
+                    while pp1 != NULL:
+                        while pp2 != NULL:
+                            if (pp1.position[] - pp2.position
+
+                    
+        ##### BIGGEST HELP IS GONNA BE ON RECORDED LECTURE DAY 4 AT AROUND 29:20
+        ##### code in textbook is on page 42
+        # if 
         # positional merge: first check for doc ids that all of the postings share, next 
-        # basically two for loops 
         # TODO: program this method. Retrieve the postings for the individual terms in the phrase,
 		# and positional merge them together.
 
