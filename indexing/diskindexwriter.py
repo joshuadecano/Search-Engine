490760930aecda7a586ef96179fb816e73e04191
joshuadecano <joshuadecano@gmail.com>
2022-10-30 16:58:29 -0700

    diskwriter index just needs one thing
    i hope it runs properly, i need to ask about path in office hours

--- indexing/diskindexwriter.py
+++ indexing/diskindexwriter.py
@@ -1,8 +1,26 @@
 from indexing import Index, PositionalInvertedIndex
 from pathlib import Path
 import struct
-def write_index(pi = PositionalInvertedIndex):
+import sqlite3
+def write_index(pi : PositionalInvertedIndex):
     f = open("postings.bin","wb")
     vocab = pi.vocabulary()
+    connection = sqlite3.connect("")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE bytes (position INTEGER)")
+    add_byte_position = ("INSERT INTO bytes "
+                        "(position) "
+                        "VALUES (%(position)s)")
     for s in vocab:
         post = pi.get_postings(s)
+        f.write(struct.pack(len(post)))
+        for t in post:
+            f.write(struct.pack(t.doc_id))
+            f.write(struct.pack(len(t.position)))
+            for u in t.position:
+                f.write(struct.pack(u))
+        f.seek(len(post))
+        inputs = {
+            'position': f.tell()
+        }
+        cursor.execute(add_byte_position, inputs)
