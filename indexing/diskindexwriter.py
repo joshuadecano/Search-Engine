08d64aef803953705cb0741f4048b2a3816c133e
joshuadecano <joshuadecano@gmail.com>
2022-10-31 19:42:49 -0700

    finished diskindexwriter with path and gap

--- indexing/diskindexwriter.py
+++ indexing/diskindexwriter.py
@@ -4,7 +4,7 @@ import struct
 import sqlite3
 def write_index(pi : PositionalInvertedIndex, deva_path : Path):
     asura_path = deva_path + "postings.bin"
-    f = open(asura_path,"wb")
+    f = open(asura_path,"rb")
     vocab = pi.vocabulary()
     connection = sqlite3.connect("bytepositions.db")
     cursor = connection.cursor()
@@ -15,11 +15,15 @@ def write_index(pi : PositionalInvertedIndex, deva_path : Path):
     for s in vocab:
         post = pi.get_postings(s)
         f.write(struct.pack(len(post)))
+        prev_docid = 0
         for t in post:
-            f.write(struct.pack(t.doc_id))
+            f.write(struct.pack(t.doc_id - prev_docid))
             f.write(struct.pack(len(t.position)))
+            prev_docid = post[t]        # stores the current doc_id for the gap
+            prev_position = 0
             for u in t.position:
-                f.write(struct.pack(u))
+                f.write(struct.pack(u - prev_position))
+                prev_position = u   # stores the current position for the gap
         f.seek(len(post))
         inputs = {
             'position': f.tell()
--- indexing/diskpositionalindex.py
+++ indexing/diskpositionalindex.py
@@ -6,15 +6,16 @@ import struct
 import sqlite3
 
 class DiskPositionalIndex(Index):
-    def __init__(self, path: Path, vocab : Iterable[str], corpus_size : int):
+    def __init__(self, deva_path: Path, vocab : Iterable[str], corpus_size : int):
         self.vocabulary = list(vocab)
         self.vocabulary.sort()
         self.corpus_size = corpus_size
         self.connection = sqlite3.connect("bytepositions.db")
-    #def get_postings(self, term : str) -> Iterable[Posting]:
-    #    connection = sqlite3.connect("bytepositions.db")
-    #    cursor = connection.cursor()
-    #    target_byte = 
-    #def add_term(self, term : str, doc_id : int):
-    #def vocabulary(self) -> Iterable[str]:
-    #    return self.vocabulary
+        self.path = deva_path
+    def get_postings(self, term : str) -> Iterable[Posting]:
+        connection = sqlite3.connect("bytepositions.db")
+        cursor = connection.cursor()
+        target_byte = 
+    def add_term(self, term : str, doc_id : int):
+    def vocabulary(self) -> Iterable[str]:
+        return self.vocabulary
