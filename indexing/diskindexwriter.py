eda2479f9a2ae70d8fd3bca843968b052bf918e7
joshuadecano <joshuadecano@gmail.com>
2022-10-31 22:40:06 -0700

    need to calculate document weight
    how do i do it in order of documents? hmm

--- indexing/diskindexwriter.py
+++ indexing/diskindexwriter.py
@@ -2,30 +2,42 @@ from indexing import Index, PositionalInvertedIndex
 from pathlib import Path
 import struct
 import sqlite3
+import numpy as np
 def write_index(pi : PositionalInvertedIndex, deva_path : Path):
+    hashmap = {}
     asura_path = deva_path + "postings.bin"
     f = open(asura_path,"rb")
     vocab = pi.vocabulary()
     connection = sqlite3.connect("bytepositions.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE bytes (position INTEGER)")
+    cursor.execute("CREATE TABLE bytes (terms TEXT, position INTEGER)")
     add_byte_position = ("INSERT INTO bytes "
-                        "(position) "
-                        "VALUES (%(position)s)")
-    for s in vocab:
+                        "(terms, position) "
+                        "VALUES (%(term)s ,%(position)s)")
+    for s in vocab: #for each term in vocab
         post = pi.get_postings(s)
-        f.write(struct.pack(len(post)))
+        f.write(struct.pack('i', len(post)))
         prev_docid = 0
-        for t in post:
-            f.write(struct.pack(t.doc_id - prev_docid))
-            f.write(struct.pack(len(t.position)))
+        occurences = 0
+        for t in post:  # for each posting with term s
+            f.write(struct.pack('i', (t.doc_id - prev_docid)))
+            f.write(struct.pack('i', len(t.position)))
             prev_docid = post[t]        # stores the current doc_id for the gap
             prev_position = 0
+            occurences = len(t.position)
             for u in t.position:
-                f.write(struct.pack(u - prev_position))
+                f.write(struct.pack('i', (u - prev_position)))
                 prev_position = u   # stores the current position for the gap
-        f.seek(len(post))
+
+            hashmap[s] = occurences     # key = term, value = tftd
+            preta_path = deva_path + "docWeights.bin"
+            g = open(preta_path,"rb")
+            g.write(struct.pack('d', ld))
+            
+        f.seek(len(post))   
         inputs = {
+            'terms' : s,
             'position': f.tell()
-        }
+        }    
+        f.seek(0,2) #move the file pointer to the end of a file
         cursor.execute(add_byte_position, inputs)
--- indexing/diskpositionalindex.py
+++ indexing/diskpositionalindex.py
@@ -11,11 +11,32 @@ class DiskPositionalIndex(Index):
         self.vocabulary.sort()
         self.corpus_size = corpus_size
         self.connection = sqlite3.connect("bytepositions.db")
-        self.path = deva_path
+        self.path = open(deva_path,"rb")
     def get_postings(self, term : str) -> Iterable[Posting]:
         connection = sqlite3.connect("bytepositions.db")
         cursor = connection.cursor()
-        target_byte = 
-    def add_term(self, term : str, doc_id : int):
+        target_byte = cursor.execute("SELECT position FROM bytes WHERE terms = (?)", term)
+        self.path.seek(target_byte) # seek to the target_byte given by the query
+        size = int(self.path.read(1))   # DFt
+                                        # it will be read as a binary number and we need to set it to int
+        posting_list = [None] * size # posting list we will return
+        prev_docid = 0
+        for s in size:  # for each doc containing the term
+            self.path.seek(1,1) # seek to doc_id
+            doc_id = struct.unpack('i', self.path.read(1)) #doc_id
+            post = Posting(doc_id + prev_docid) #create a posting with the doc_id
+            prev_docid = doc_id
+            self.path.seek(1,1) # seek to tftd (number of positions)
+            position_count = size = struct.unpack('i', self.path.read(1))
+            prev_position = 0
+            for t in position_count:
+                self.path.seek(1,1) # seek to pi, (position of term in doc_id)
+                position = struct.unpack('i', self.path.read(1))
+                post.add_position(position + prev_position)
+                prev_position = position
+            posting_list.append(post)
+        return posting_list
+    #def add_term(self, term : str, doc_id : int):
+    # need to think if we need this
     def vocabulary(self) -> Iterable[str]:
         return self.vocabulary
